189d45c97c0bce8b2ece91c34a3f3056b6d1f4a4
aleksandrblagov <blagov13rus@gmail.com>
2024-05-09 18:15:09 +0300

    4. Создание вакансии [#504849]

--- src/main/java/ru/job4j/dreamjob/model/Vacancy.java
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -7,13 +7,15 @@ public class Vacancy {
     private int id;
     private String title;
     private String description;
-    private final LocalDateTime creationDate;
+    private final LocalDateTime creationDate = LocalDateTime.now();
 
     public Vacancy(int id, String title, String description, LocalDateTime creationDate) {
         this.id = id;
         this.title = title;
         this.description = description;
-        this.creationDate = creationDate;
+    }
+
+    public Vacancy() {
     }
 
     public String getDescription() {
